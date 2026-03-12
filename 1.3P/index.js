c97971513229f43737f77e3fd86f99169b72eef4
omjagtap100 <om.jagtap@tribox.me>
2026-03-12 17:47:02 +1100

    1.3P

---
+++ 1.3P/index.js
@@ -0,0 +1,12 @@
+const studentName = "Om Jagtap";
+const studentID = "s225435163";
+
+console.log("Welcome to SIT725!");
+console.log("Name: " + studentName);
+console.log("Student ID: " + studentID);
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+console.log("Sum of 5 and 10 is: " + addNumbers(5, 10));
